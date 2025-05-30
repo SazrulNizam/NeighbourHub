b73ddf26ee731048176b1eb7cdcd8335e3157891
SazrulNizam <sazrulnizam999@gmail.com>
2025-05-30 17:30:38 +0800

    login page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,19 +12,54 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js',  'resources/assets/css/style.css',
+    'resources/assets/fonts/material.css',
+    'resources/assets/fonts/fontawesome.css',
+    'resources/assets/fonts/feather.css',
+    'resources/assets/fonts/tabler-icons.min.css',
+    'resources/assets/fonts/phosphor/duotone/style.css',
+    'resources/assets/js/script.js',])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+            <!-- [ Pre-loader ] start -->
+    <div class="loader-bg fixed inset-0 bg-white dark:bg-themedark-cardbg z-[1034]">
+      <div class="loader-track h-[5px] w-full inline-block absolute overflow-hidden top-0">
+        <div class="loader-fill w-[300px] h-[5px] bg-primary-500 absolute top-0 left-0 animate-[hitZak_0.6s_ease-in-out_infinite_alternate]"></div>
+      </div>
+    </div>
+    <!-- [ Pre-loader ] End -->
+            <div class="auth-main relative">
+      <div class="auth-wrapper v1 flex items-center w-full h-full min-h-screen">
+        <div class="auth-form flex items-center justify-center grow flex-col min-h-screen relative p-6 ">
+          <div class="w-full max-w-[350px] relative">
+            <div class="auth-bg ">
+              <span class="absolute top-[-100px] right-[-100px] w-[300px] h-[300px] block rounded-full bg-theme-bg-1 animate-[floating_7s_infinite]"></span>
+              <span class="absolute top-[150px] right-[-150px] w-5 h-5 block rounded-full bg-primary-500 animate-[floating_9s_infinite]"></span>
+              <span class="absolute left-[-150px] bottom-[150px] w-5 h-5 block rounded-full bg-theme-bg-1 animate-[floating_7s_infinite]"></span>
+              <span class="absolute left-[-100px] bottom-[-100px] w-[300px] h-[300px] block rounded-full bg-theme-bg-2 animate-[floating_9s_infinite]"></span>
+            </div>  
+           <div class="card sm:my-12  w-full shadow-none">
+              <div class="card-body !p-10">
+                <div class="text-center mb-8">
+                  <a href="#"><img src="../assets/images/logo-dark.svg" alt="img" class="mx-auto auth-logo"/></a>
+                </div>
+                <h4 class="text-center font-medium mb-4">Login</h4>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="">
                 {{ $slot }}
             </div>
-        </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
     </body>
+    <script src="{{ asset('assets/js/plugins/simplebar.min.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/icon/custom-icon.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/feather.min.js') }}"></script>
+<script src="{{ asset('assets/js/component.js') }}"></script>
+<script src="{{ asset('assets/js/theme.js') }}"></script>
 </html>
